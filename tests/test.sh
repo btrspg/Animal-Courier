4b5fe0d81718049d4dee3a40860f54f8567bf473
chenyl <yuelong_chen@yahoo.com>
2019-01-11 13:23:55 +0800

    测试脚本之前是错的

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<100000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(1);'
-perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<100000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(1);'
-perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<100000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(1);'
-perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<100000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(1);'
+perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<10000000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
+perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<10000000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
+perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<10000000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
+perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<10000000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
