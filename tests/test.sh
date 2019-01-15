40b60dd7fd15061c7a758dfdb24727636f5edd3e
chenyl <yuelong_chen@yahoo.com>
2019-01-15 10:18:37 +0800

    测试集修改，减少测试时间

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<10000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
+perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<20000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
+perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<30000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
+perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<40000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
+perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<50000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
+perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<60000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
+perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<70000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
+perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<80000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
+perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<90000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
 perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<100000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
-perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<1000000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
-perl -e 'for(my $i=0;$i<2;$i++){for(my $j=0;$j<10000000;$j++){$sh{"$i,$j"}=$i+$j;}sleep(2);}'
